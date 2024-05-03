fefb4e785557521cc7073bdbf125eb4404e3ecc9
vpriyanshu671 <vpriyanshu671@gmail.com>
2024-05-03 13:20:32 +0530

    Fix : delete about.html and add white paragraph in index.html

--- app/routes.py
+++ app/routes.py
@@ -5,14 +5,10 @@ from flask import render_template
 def index():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
 @app.route('/projects')
 def projects():
     return render_template('projects.html')
 
-@app.route('/contact')
-def contact():
-    return render_template('contact.html')
+@app.route('/cv')
+def cv():
+    return render_template('cv.html')
